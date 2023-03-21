3c25495978eb17766af9a431e99ceaa4bc0b3ebe
Roman <rprikhoda@ukr.net>
2023-03-21 16:08:57 +0200

    change search params

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,12 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
-const searchParams = new URLSearchParams({
-  fields: 'name,capital,population,flags,languages',
-});
+// const searchParams = new URLSearchParams({
+//   fields: 'name,capital,population,flags,languages',
+// });
 
 export function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}?${searchParams}`).then(request => {
+  return fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(request => {
     if (!request.ok) {
       throw new Error(request.status);
     }
